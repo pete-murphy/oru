e275660811087d4db179ec51a079b2fb232da8e3
Peter Murphy <26548438+ptrfrncsmrph@users.noreply.github.com>
2023-09-30 20:58:56 -0400

    feat(frontend): npm run dev

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,5 +4,16 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss/plugin")(({ addBase }) => {
+      addBase({
+        '[type="search"]::-webkit-search-decoration': { display: "none" },
+        '[type="search"]::-webkit-search-cancel-button': { display: "none" },
+        '[type="search"]::-webkit-search-results-button': { display: "none" },
+        '[type="search"]::-webkit-search-results-decoration': {
+          display: "none",
+        },
+      });
+    }),
+  ],
 };
